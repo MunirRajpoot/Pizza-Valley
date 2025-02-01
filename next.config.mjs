a4f7ff1340faf6ea1761c00521f391490a40c8e5
Muneer Rasool <113513828+MunirRajpoot@users.noreply.github.com>
2025-02-01 21:16:39 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
+    NEXT_PUBLIC_BASE_URL: "https://pizza-valley.vercel.app/",
   },
   images: {
     remotePatterns: [
