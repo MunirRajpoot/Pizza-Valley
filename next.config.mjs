1637fdf5bcc49b7a6e83a192dd13a5283b39436b
MunirRajpoot <munirrajpoot1012@gmail.com>
2025-02-01 21:09:20 +0500

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
+  },
   images: {
     remotePatterns: [
       {
